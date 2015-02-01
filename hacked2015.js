25b1e63985f96768980df21c1576b71644a127c0
Stephanie Gil <sgil@ualberta.ca>
2015-01-31 17:50:27 -0800

    start of integration between meteor and iron router

--- hacked2015.js
+++ hacked2015.js
@@ -1,20 +1,69 @@
+// routing
+Router.map(function() {
+  this.route('home', {path: '/'});
+  this.route('dashboard', {path: '/'});
+});
+
+
+
 // db for events
 Events = new Mongo.Collection("events");
+Clubs = new Mongo.Collection("clubs");
 
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  var timestamp = new Date();
+  var today = new Date(timestamp.getFullYear(),
+                      timestamp.getMonth(),
+                      timestamp.getDate());
+
   Template.body.helpers({
-      events: [
-        { name: "free hotdogs", date: "feb 2"},
-        { name: "hackathon", date: "jan 31"}
-      ]
-      
-      //  events: function () {
-  
-      //return Events.find({});
+      events: function () { return Events.find();
+      }
   });
+/*
+  // register student club
+  Template.register.events({
+      var club =  event.target.club.value;
+      var email = event.target.email.value;
+      var university = event.target.university.value;
+
+      Clubs.insert({
+          club: club,
+          university: university,
+          email: email 
+ 
+          return false;
+      });
+  });
+
+  // adding a new event
+  Template.dashboard.events({
+      "submit .new-event": function (event) {
+        var name = event.target.text.value;
+        var date = event.target.date.value;
+        var club = event.target.club.value;
+
+        Events.insert({
+            name: name,
+            date: date,
+            club: club
+        });
+
+        event.target.name.value = "";
+        event.target.date.value = "";
+        event.target.club.value = "";
+
+        return false;
+      }
+  });
+
+  Template.datepicker.rendered=function() {
+    this.$('datepicker').datepicker();
+  };
+  */
 }
 
 if (Meteor.isServer) {
